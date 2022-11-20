b06c87eb69d3a1741f5b339e9669203759190d0f
Lucas Barreto <lucas_barreto29@outlook.com>
2022-11-20 18:29:21 -0300

    adicionada navegacao ao clicar no botao

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import './HomePage.css';
 
 export function HomePage() {
+
+    const navigate = useNavigate();
+
     return (
         <div className='home-page-container'>
-            <button>Começar!</button>
+            <button onClick={() => navigate('/1st')} className='StartButton'>Começar!</button>
         </div>
     )
 
