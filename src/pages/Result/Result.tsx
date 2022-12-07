8ad1eb661f8b5562cfe5eb05688caa0ed7467d45
Lucas Barreto <lucas_barreto29@outlook.com>
2022-12-07 19:10:32 -0300

    mudada estilizacao

--- src/pages/Result/Result.tsx
+++ src/pages/Result/Result.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { API_URL } from '../../config/env'
+import "./Result.css"
 
 function Result() {
     const hero = JSON.parse(localStorage.getItem("hero") as string)
@@ -10,8 +11,12 @@ function Result() {
     }, [])
 
     return (
-        <div>
-            <img src={`${API_URL}/images/${hero.profilePicture}`} alt="" />
+        <div className='result'>
+            <div className='result-container'>
+                <h1>Seu herói é o:</h1>
+                <img src={`${API_URL}/images/${hero.profilePicture}`} alt="" />
+                <h1>{hero.name}</h1>
+            </div>
         </div>
     )
 }
